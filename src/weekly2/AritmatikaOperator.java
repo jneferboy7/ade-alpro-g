515f4ae0236eb0a1072644da0e44eda07cf021e8
wawanindrawan <wawan.jnefer@gmail.com>
2021-10-24 21:38:42 +0800

    update kedua

--- src/weekly2/AritmatikaOperator.java
+++ src/weekly2/AritmatikaOperator.java
@@ -6,8 +6,8 @@ public class AritmatikaOperator {
     public static void main(String[] args) {
         int nilai1,nilai2;
         boolean hasil = true;
-        nilai1 = 4;
-        nilai2 = 5;
+        nilai1 = 10;
+        nilai2 = 20;
         
         if (nilai1 < 5 || nilai2 > 10) {
             System.out.println(hasil);
--- src/weekly2/charOperator.java
+++ src/weekly2/charOperator.java
@@ -13,6 +13,7 @@ public class charOperator {
     public static void main(String[] args) {
         char simbol = '\u0085';
         char simbol2 = '\u00BD';
+        char simbol3 = '\u0000';
         
         System.out.println("Simbol = " + simbol);
         System.out.println("Simbol = " + simbol2);
